ae8e58ee0b68b63678f5db2a53c066c287af4d79
viktor <v.dome1991@gmail.com>
2021-08-24 13:15:58 +0200

    [add] containers and video to main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { VideoContainer, MainContentContainer } from "./layout/PageElements";
+import video from '../../src/static/Artemis-logo-reveal-1080p.mp4';
+import Content from "./Content";
+import { SlideMainContainer } from "./layout/SlidePageElements";
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <MainContentContainer>
+      <VideoContainer>
+        <video autoPlay muted>
+          <source src={video} type="video/mp4"></source>
+        </video>
+      </VideoContainer>
+      <SlideMainContainer >
+        <Content />
+      </SlideMainContainer>
+    </MainContentContainer>
   );
 };
 
