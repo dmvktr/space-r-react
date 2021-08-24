3dd87980b38c04dccc7c62e57779cc7096f32303
viktor <v.dome1991@gmail.com>
2021-08-24 13:14:13 +0200

    [update] News to include articles component

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { NewsContainer } from "./layout/PageElements";
+import Articles from './Articles';
 
 const News = () => {
   return (
-    <div>
-      <h1>news</h1>
-    </div>
+    <NewsContainer>
+      <Articles />
+    </NewsContainer>
   );
 };
 
