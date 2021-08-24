62775f51920eed05d1666ace8e03c9fdd25ba80a
viktor <v.dome1991@gmail.com>
2021-08-24 13:12:40 +0200

    [add] styled main page containers

--- src/components/layout/PageElements.js
+++ src/components/layout/PageElements.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
   display: grid;
   width: 100vw;
-  height: 100vh;
+  /* height: 100vh; */
   grid-template-columns: 5% 90% 5%;
   grid-template-rows: 5% 90% 5%;
   grid-template-areas:
@@ -11,3 +11,22 @@ export const MainContainer = styled.div`
     "content content content"
     "footer footer footer";
 `;
+
+export const MainContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const NewsContainer = styled.div`
+    grid-area: content;
+    text-align: center;
+`;
+
+export const VideoContainer = styled.div`
+    width: 100vw;
+`
+
+export const SlideContainer = styled.div`
+    width: 100vw;
+    display: flex;
+`
