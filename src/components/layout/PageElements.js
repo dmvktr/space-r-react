ab871a1e9dcb4c6ba4622dff17487e1df1d174a0
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-24 12:05:47 +0200

    [update] MainContainer grid style

--- src/components/layout/PageElements.js
+++ src/components/layout/PageElements.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-
 export const MainContainer = styled.div`
-    display: grid;
-    width: 100vw;
-    grid-template-columns: 5% 90% 5%;
-    grid-template-rows: 20% 40% 39%;
-    grid-template-areas:
-        "header header header"
-        ". content ."
-        ". content . ";
-`
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  grid-template-columns: 5% 90% 5%;
+  grid-template-rows: 5% 90% 5%;
+  grid-template-areas:
+    "header header header"
+    "content content content"
+    "footer footer footer";
+`;
