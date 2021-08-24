9b810a872e293a01a91e5139e86238a922d88109
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-24 12:05:16 +0200

    [update] NavContainer style

--- src/components/layout/NavBarElements.js
+++ src/components/layout/NavBarElements.js
@@ -1,8 +1,11 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
+import { NavLink as Link } from "react-router-dom";
 
 export const NavContainer = styled.div`
-    display: grid;
-    grid-template-columns: 10% 80% 10%;
-    grid-area: header;
-`
+  display: grid;
+  grid-template-columns: 10% 80% 10%;
+  grid-area: header;
+  border-bottom: 3px solid black;
+`;
+
+export const NavbarOption = styled(Link)``;
