b7831cf0ccdc098b587de6f748a308cf50c4cf0d
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-24 12:03:31 +0200

    [add] links to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { NavContainer } from './layout/NavBarElements';
-
+import React from "react";
+import { NavContainer } from "./layout/NavBarElements";
+import styled from "styled-components";
+import { NavbarOption } from "./layout/NavBarElements";
 function NavBar() {
-    return (
-        <NavContainer/>
-    )
+  return (
+    <NavContainer>
+      <Logo />
+      <Links>
+        <NavbarOption to="/" title="Home" theme={""}>
+          Home
+        </NavbarOption>
+
+        <NavbarOption to="/news" title="News" theme={""}>
+          News
+        </NavbarOption>
+
+        <NavbarOption to="/gallery" title="Gallery" theme={""}>
+          Gallery
+        </NavbarOption>
+
+        <NavbarOption to="/astronauts" title="Astronauts" theme={""}>
+          Astronauts
+        </NavbarOption>
+
+        <NavbarOption to="/locations" title="Locations" theme={""}>
+          Locations
+        </NavbarOption>
+      </Links>
+    </NavContainer>
+  );
 }
 
-export default NavBar
+const Logo = styled.div``;
+const Links = styled.div`
+  display: flex;
+  gap: 4rem;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export default NavBar;
