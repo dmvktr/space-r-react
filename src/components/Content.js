a2fe2ae697636818c794434ab87b321922bb78df
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-24 14:58:00 +0200

    [add] Lorem ipsum for testing

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,17 +4,30 @@ import { SlideMainContainer } from "./layout/SlidePageElements";
 
 const Content = () => {
   return (
-        <>
-        <PageContent>
-            <SlideMainContainer >
-                <h1>Test</h1>
-            </SlideMainContainer>
-        </PageContent>
-        </>
-    );
+    <>
+      <PageContent>
+        <SlideMainContainer>
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id
+            felis sit amet nisi vulputate vehicula. Suspendisse eget imperdiet
+            tortor, ut suscipit diam. Integer blandit interdum eros, vel
+            tincidunt nulla. Aenean semper faucibus sapien sed sollicitudin.
+            Mauris fermentum urna sit amet molestie tempus. Sed fringilla nisi
+            id eros feugiat aliquet. Pellentesque condimentum neque sed enim
+            aliquam vehicula. Suspendisse placerat risus velit, a dictum ligula
+            interdum nec. Integer hendrerit velit sed cursus egestas. Vestibulum
+            lacus arcu, auctor eu iaculis sit amet, egestas id nibh. Etiam eget
+            sem libero. Etiam id mattis eros. Proin eu nisi quis libero dictum
+            consequat quis id sem. Ut nec lacus dolor.{" "}
+          </h1>
+        </SlideMainContainer>
+      </PageContent>
+    </>
+  );
 };
 
 const PageContent = styled.div`
   grid-area: content;
+  margin: 1vh 0;
 `;
 export default Content;
