aa70af197b858a4cda8ad864ab4dfc0bb9a07061
viktor <v.dome1991@gmail.com>
2021-08-25 14:41:54 +0200

    [add] fetch and real data buildup to Astronauts

--- src/components/Astronauts.js
+++ src/components/Astronauts.js
@@ -1,103 +1,74 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Astronaut from "./Astronaut";
 import { AstronautMainContainer,
     AstronautCardContainer,
     AstronautCardsMainContainer,
     AstronautsPageText,
-    CardTop,
-    CardImageContainer,
-    CardPersonalDetail,
-    Card,
-    AstronautImage,
-    CardBio } from "./layout/AstronautElements";
+    Error } from "./layout/AstronautElements";
 
-const Astronauts = () => {
+const Astronauts = (props) => {
   const baseEndpoint = "https://ll.thespacedevs.com/2.2.0/astronaut/";
   const fetchLimit = 2;
   const offset = 0;
 
-//   const [astronauts, setAstronauts] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState('');
-
+  const [astronauts, setAstronauts] = useState({
+    next: null,
+    previous: null,
+    results: [],
+  });
+  const [error, setError] = useState('');
   
-//   const fetchData = () => {
-//     axios
-//         .get(`${baseEndpoint}order=${fetchLimit}&offset=${offset}`)
-//         .then((res) => {
-//           setAstronauts(res.data.results);
-//         })
-//         .catch((err) => {
-//             setError(err);
-//         })
-//         .finally(() => {
-//             setLoading(false);
-//         });
-// };
 
-// useEffect(() => {
-//     fetchData()
-//   }, [])
+
+  const assembleTargetURL = () => {
+    return `${baseEndpoint}?format=json&limit=${fetchLimit}&offset=${offset}`;
+  };
+
+  useEffect(() => {
+    axios
+        .get(assembleTargetURL())
+        .then((res) => {
+          setAstronauts({
+            next: res.data.next,
+            previous: res.data.previous,
+            results: res.data.results
+          });
+        })
+        .catch((err) => {
+          setError(err.message)
+        })
+    }, []);
 
   return (
-    <>
       <AstronautMainContainer>
       <AstronautsPageText>
         Astronauts
       </AstronautsPageText>
+      {error ? (
+            <Error>
+              An error occured, while fetching the astronauts information. Please try again later!
+            </Error>
+        ) : (
         <AstronautCardsMainContainer>
+          {astronauts.results.map((astronaut) => (
           <AstronautCardContainer>
-            <Card id="card">
-              <CardTop />
-              <CardImageContainer>
-                <AstronautImage />
-              </CardImageContainer>
-              <CardPersonalDetail id="astronautName">
-                Albert Woofers
-              </CardPersonalDetail>
-              <CardPersonalDetail>
-                Date of birth: 2000.11.01
-              </CardPersonalDetail>
-              <CardPersonalDetail>
-                Nationality: ENG
-              </CardPersonalDetail> 
-              <CardPersonalDetail>
-                Mission: ???
-              </CardPersonalDetail>
-              <CardBio>
-                Lorem ipsum dolor sit amet ......
-              </CardBio>
-            </Card>
-            </AstronautCardContainer>
-            <AstronautCardContainer>
-            <Card id="card">
-              <CardTop />
-              <CardImageContainer>
-                <AstronautImage />
-              </CardImageContainer>
-              <CardPersonalDetail id="astronautName">
-                Moofers McGoofers
-              </CardPersonalDetail>
-              <CardPersonalDetail>
-                Date of birth: 1980.01.01
-              </CardPersonalDetail>
-              <CardPersonalDetail>
-                Nationality: AUT
-              </CardPersonalDetail> 
-              <CardPersonalDetail>
-                Mission: Soyuz-U2
-              </CardPersonalDetail>
-              <CardBio>
-                Lorem ipsum dolor sit amet ......
-              </CardBio>
-            </Card>
-          </AstronautCardContainer>
-          <AstronautCardContainer />
-          <AstronautCardContainer />
-        </AstronautCardsMainContainer>
+            <Astronaut
+          key={astronaut.id}
+          name={astronaut.name}
+          picture={astronaut.profile_image_thumbnail}
+          theme={props.theme}
+          dob={astronaut.date_of_birth}
+          nationality={astronaut.nationality}
+          status={astronaut.status.name}
+          bio={astronaut.bio}
+          id="card"></Astronaut>
+        </AstronautCardContainer>
+      ))}
+      </AstronautCardsMainContainer>
+      )}
       </AstronautMainContainer>
-    </>
   );
 };
 
