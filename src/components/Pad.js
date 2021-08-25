fd5f9dce6a651128ede8f1df143dabe1096b8b8a
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-25 20:15:15 +0200

    [fix] pads image html tag

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { PadsContainer } from './layout/PageElements';
+import React from "react";
+import { PadsContainer } from "./layout/PageElements";
+import styled from "styled-components";
 
 export const Pad = (props) => {
+  const { name, wiki_url, map_url, latitude, longitude, total_launch_count } =
+    props.pad;
 
-    const { name, wiki_url, map_url, latitude, longitude, total_launch_count } = props.pad;
-    
-    return (
-        <PadsContainer>
-            <img src={map_url} alt="pad-location-on-google-maps"/>
-            <p>For more information about the pad, you can visit the wikipedia site:
-            <a href={wiki_url}>
-                {name}
-            </a>
-            </p>
-            <p>Longitude: {longitude}  |  Latitude: {latitude}</p>
-            <p>Total launch count: {total_launch_count}</p>
-        </PadsContainer>
-    )
-}
+  return (
+    <PadsContainer>
+      <ImageDiv src={map_url} alt="pad-location-on-google-maps" />
+      <p>
+        For more information about the pad, you can visit the wikipedia site:
+        <a href={wiki_url}>{name}</a>
+      </p>
+      <p>
+        Longitude: {longitude} | Latitude: {latitude}
+      </p>
+      <p>Total launch count: {total_launch_count}</p>
+    </PadsContainer>
+  );
+};
+
+const ImageDiv = styled.div`
+  content: ${(props) => props.src};
+  width: 10vw;
+  height: 10vw;
+`;
