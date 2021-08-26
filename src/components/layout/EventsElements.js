b99501458cb55455341071d2c1dd913da178a11f
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-26 15:55:22 +0200

    [fix] elements responsiveness

--- src/components/layout/EventsElements.js
+++ src/components/layout/EventsElements.js
@@ -2,19 +2,20 @@ import styled, { keyframes } from "styled-components";
 
 export const EventsPageContainer = styled.div`
   display: flex;
+  grid-area: content;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-width: 100vw;
+  width: 100vw;
 `;
 
 export const EventsContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 5vh;
+  margin: auto;
   width: 70vw;
-  height: auto;
+  gap: 2vw;
 `;
 
 const ScaleUp = keyframes`
@@ -32,11 +33,10 @@ const ScaleDown = keyframes`
 export const SingleEventContainer = styled.div`
   display: grid;
   grid-template-columns: 1.4fr 0.6fr 1fr 1fr;
-  grid-template-rows: 5% 20% 50% 20%;
+  grid-template-rows: 10% 15% auto 20%;
   background: white;
   border: 2px solid black;
   gap: 0.5rem;
-  height: 100%;
   border-radius: 20px;
   color: black;
   transition: ${ScaleDown} 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
@@ -65,51 +65,54 @@ export const DateContainer = styled.h3`
 export const Title = styled.h3`
   grid-area: title;
   color: rgb(60 72 88);
-  padding: 1rem;
+  padding: 1.4rem;
   font-size: 1.5rem;
 `;
 export const Description = styled.h3`
   grid-area: description;
   color: rgb(153 153 153);
-  font-size: 1rem;
+  font-size: 1.2rem;
   font-weight: 700;
   font-family: "Roboto Slab", "Times New Roman", serif;
-  line-height: 1.4em;
   padding: 1rem;
+  max-height: 20vh;
+  overflow: hidden;
 `;
 export const Location = styled.h3`
   grid-area: location;
+  display: flex;
+  align-items: center;
   color: white;
-  padding: 20px;
+  padding: 4px;
   text-transform: uppercase;
   font-size: 10px;
   background-color: rgb(33 150 243);
-  margin: auto;
 `;
 export const EventId = styled.h3`
   grid-area: id;
   display: flex;
   justify-content: flex-end;
   color: cornflowerblue;
-  padding: 1rem;
+  padding: 0.55rem 1rem;
 `;
 export const EventUrl = styled.a`
   display: flex;
   justify-content: flex-end;
   align-items: center;
   grid-area: url;
-  padding: 1rem;
+  padding: 0.25rem 1rem;
 `;
 
 export const ThumbnailImg = styled.div`
   display: flex;
   grid-area: picture;
+  align-items: center;
   content: url(${(props) => props.picture});
-  background-size: cover;
+  background-size: 40vw auto;
   margin: auto;
   background-position: center;
   background-repeat: no-repeat;
-  padding: 1rem;
+  padding: 0.4rem;
   border: 2px solid rgba(0, 0, 0, 0.3);
-  border-radius: 5px;
+  border-radius: 20px;
 `;
