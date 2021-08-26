8dfb852b9c6599fe7bc0e8f2e9871a68dad786c5
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-26 12:33:34 +0200

    [refactor] move all styled elements to GalleryElements.js

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import {
+  Thumbnail,
+  Title,
+  Card,
+  Date,
+  CloseButton,
+  HdImg,
+} from "./layout/GalleryElements";
+
 import Modal from "react-modal";
 import LoadingAnim from "../static/img/loading.gif";
 
@@ -53,70 +61,5 @@ const GalleryCard = (props) => {
     </React.Fragment>
   );
 };
-
-const FocusAnim = keyframes`
-  0% {
-    filter: blur(12px);
-    opacity: 0;
-  }
-  100% {
-    filter: blur(0px);
-    opacity: 1;
-  }
-`;
-
-const Thumbnail = styled.div`
-  background-image: url(${(props) => props.picture});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 100%;
-  height: 100%;
-`;
-const CloseButton = styled.button`
-  float: right;
-  color: white;
-  font-size: 2rem;
-  font-weight: 800;
-  background-color: black;
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  border: 2px solid cornflowerblue;
-`;
-const Title = styled.h4`
-  opacity: 0;
-  text-align: center;
-  position: relative;
-  height: 1em;
-  top: -30px;
-  z-index: 40;
-  font-size: 20px;
-  color: white;
-  overflow: hidden;
-`;
-
-const Card = styled.div`
-  width: auto;
-  height: 15vh;
-  &:hover ${Title} {
-    opacity: 100;
-    animation: ${FocusAnim} 0.5s cubic-bezier(0.55, 0.085, 0.68, 0.53) both;
-  }
-  cursor: pointer;
-  background-image: url(${LoadingAnim});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-`;
-const HdImg = styled.div`
-  background-image: url(${(props) => props.picture});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 90vw;
-  height: 90vh;
-`;
-const Date = styled.h3``;
 
 export default GalleryCard;
