479d5c61626705cebe7eed4df98f9efddc2ac5a1
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-26 21:16:49 +0200

    [update] container styles

--- src/components/layout/SlidePageElements.js
+++ src/components/layout/SlidePageElements.js
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const SlideMainContainer = styled.div`
-    display: flex;
-    min-width: 100vw;
-    min-height: 40vh;
-    max-height: 80vh;
-    height: 80vh;
+  display: flex;
+  width: 100vw;
+  min-height: 60vh;
 `;
 
-export const SlideContainer = styled.div`
+export const SlidePresentationContainer = styled.div`
   width: 100vw;
-  display: flex;
-  content: '';
-  background-image: url(${(props)=> props.image});
+  height: 100vh;
+  background-image: url(${(props) => props.image});
   background-repeat: no-repeat;
   background-position: center;
   background-size: contain;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  border-top: 1px solid rgba(0, 0, 0, 0.5);
+  margin: 0 10px;
 `;
