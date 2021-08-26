f9eb4f49a8f2b972ee60f097bfaaf245988dfeb9
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-08-26 11:30:57 +0200

    [Add] some card style for text

--- src/components/Location.js
+++ src/components/Location.js
@@ -7,25 +7,34 @@ export const Location = (props) => {
     const { id, name, country_code, map_image, total_launch_count, total_landing_count } = props.location;
 
     return (
-        <Link style={linkStyle} to={`/locations/location/${id}`}>
-            <LocationContainer>
+        <LocationContainer>
+            <Link style={linkStyle} to={`/locations/location/${id}`}>
                 <img style={imageStyle} src={map_image} alt="location-on-google-maps"/>
+            </Link>
+            <div style={cardDetailContainerStyle}>
                 <h3>{name}</h3>
                 <h4>{country_code}</h4>
-                <p>Launches: {total_launch_count} Landings: {total_landing_count}</p>            
-            </LocationContainer>
-        </Link>
+                <br/>
+                <p>Launches: {total_launch_count} Landings: {total_landing_count}</p>
+            </div>
+        </LocationContainer>
     )
 }
 
 const imageStyle = {
-    width: '300px',
-    height: 'auto',
+    margin: '5px',
+    maxWidth: '420px',
     boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
     borderRadius: '4%',
 }
 
-
 const linkStyle = {
     linkStyle: 'none',
+}
+
+const cardDetailContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
 }
