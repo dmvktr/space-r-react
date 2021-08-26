fecfeef3e555d8a6257c48dc0fe55128cef5795e
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-26 21:15:30 +0200

    [remove] unnecessary div elements

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-import { SlideContainer } from "./layout/SlidePageElements";
-import { SlideMainContainer } from "./layout/SlidePageElements";
+
+import { SlidePresentationContainer } from "./layout/SlidePageElements";
 
 const Content = (props) => {
-  return (
-    <>
-      <PageContent>
-        <SlideMainContainer>
-          <SlideContainer image={props.image} />
-        </SlideMainContainer>
-      </PageContent>
-    </>
-  );
+  return <SlidePresentationContainer image={props.image} />;
 };
 
-const PageContent = styled.div`
-  grid-area: content;
-  margin: 1vh 0;
-  width: 100vw;
-`;
 export default Content;
