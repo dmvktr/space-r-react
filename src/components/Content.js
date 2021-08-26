4a0485fb2e3b546ef0f12268128f299993908484
viktor <v.dome1991@gmail.com>
2021-08-26 20:28:59 +0200

    [update] content with dynamic img gain

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,25 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { SlideContainer } from "./layout/SlidePageElements";
 import { SlideMainContainer } from "./layout/SlidePageElements";
 
-const Content = () => {
+const Content = (props) => {
   return (
     <>
       <PageContent>
         <SlideMainContainer>
-          <h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id
-            felis sit amet nisi vulputate vehicula. Suspendisse eget imperdiet
-            tortor, ut suscipit diam. Integer blandit interdum eros, vel
-            tincidunt nulla. Aenean semper faucibus sapien sed sollicitudin.
-            Mauris fermentum urna sit amet molestie tempus. Sed fringilla nisi
-            id eros feugiat aliquet. Pellentesque condimentum neque sed enim
-            aliquam vehicula. Suspendisse placerat risus velit, a dictum ligula
-            interdum nec. Integer hendrerit velit sed cursus egestas. Vestibulum
-            lacus arcu, auctor eu iaculis sit amet, egestas id nibh. Etiam eget
-            sem libero. Etiam id mattis eros. Proin eu nisi quis libero dictum
-            consequat quis id sem. Ut nec lacus dolor.{" "}
-          </h1>
+          <SlideContainer image={props.image} />
         </SlideMainContainer>
       </PageContent>
     </>
