0e4d3d34cdfcec86b4cb2b32fdfdc44f42bdf44c
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-26 08:35:06 +0200

    [add] import styles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import Modal from "react-modal";
+import {
+  ArticleNewsSite,
+  ArticleContainer,
+  ArticleSummary,
+  ArticleTitle,
+  ArticleStats,
+  ArticleUrl,
+  ModalTitle,
+  ModalContainer,
+  SingleNewsELement,
+  CloseButton,
+  HdImg,
+} from "./layout/NewsElement";
 
 const customStyles = {
   content: {
@@ -10,10 +22,11 @@ const customStyles = {
     bottom: "auto",
     margin: "0",
     transform: "translate(-50%, -50%)",
-    backgroundColor: "black",
+    background: "rgb(64,64,64)",
     backgroundRepeat: "no-repeat",
     backgroundSize: "50%",
     backgroundPosition: "center",
+    borderRadius: "30px",
   },
 };
 const Articles = (props) => {
@@ -30,9 +43,9 @@ const Articles = (props) => {
   return (
     <React.Fragment>
       <SingleNewsELement onClick={openModal}>
-        <H3>{props.article.title}</H3>
+        <ArticleTitle>{props.article.title}</ArticleTitle>
         <ArticleContainer picture={props.article.imageUrl}>
-          <H4>{props.article.newsSite}</H4>
+          <ArticleNewsSite>{props.article.newsSite}</ArticleNewsSite>
         </ArticleContainer>
       </SingleNewsELement>
       <Modal
@@ -43,114 +56,24 @@ const Articles = (props) => {
         onClick={closeModal}
       >
         <ModalContainer>
-          <H2>{props.article.title}</H2>
+          <ModalTitle>{props.article.title}</ModalTitle>
           <CloseButton onClick={closeModal}>X</CloseButton>
-          <Summary>{props.article.summary}</Summary>
-          <HdImg picture={props.article.imageUrl} onClick={closeModal} />
-          <Stats>
-            {props.article.publishedAt} <br />
-            {props.article.newsSite}
+          <ArticleSummary>{props.article.summary}</ArticleSummary>
+          <ArticleUrl href={props.article.url}>
+            Source:
+            <br />
             <br />
             {props.article.url}
-          </Stats>
+          </ArticleUrl>
+          <HdImg picture={props.article.imageUrl} onClick={closeModal} />
+          <ArticleStats>
+            {props.article.publishedAt} <br />
+            {props.article.newsSite}
+          </ArticleStats>
         </ModalContainer>
       </Modal>
     </React.Fragment>
   );
 };
 
-const H4 = styled.p`
-  color: red;
-  font-weight: 600;
-  background-color: rgba(0, 0, 0, 0.4);
-`;
-
-const Summary = styled.div`
-  grid-area: summary;
-  font-weight: 600;
-  padding: 40px;
-  font-size: 1.5rem;
-`;
-
-const H3 = styled.h3`
-  background-color: black;
-`;
-
-const H2 = styled.h2`
-  grid-area: title;
-  font-weight: 600;
-  font-size: 2rem;
-  padding: 20px;
-  background-color: black;
-  color: white;
-`;
-
-const Stats = styled.div`
-  grid-area: stats;
-  padding: 20px;
-`;
-
-const ModalContainer = styled.div`
-  display: grid;
-  grid-template-columns: 50% auto 10%;
-  grid-template-rows: 20% 70% 10%;
-  width: 80vw;
-  height: 95vh;
-  justify-content: center;
-  align-items: center;
-  grid-template-areas:
-    "title title title"
-    "summary picture picture"
-    "stats stats stats";
-  background-color: white;
-`;
-
-const ArticleContainer = styled.div`
-  background-image: url(${(props) => props.picture});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 100%;
-  height: 100%;
-`;
-
-const SingleNewsELement = styled.div`
-  background-image: url(${(props) => props.picture});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 100%;
-  height: 20vw;
-  border: 3px solid white;
-  color: white;
-  display: grid;
-  grid-template-rows: 30% 70%;
-  flex: 1;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 3vh;
-  right: 2vw;
-
-  grid-area: close;
-  color: white;
-  font-size: 2rem;
-  font-weight: 800;
-  background-color: black;
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  border: 2px solid cornflowerblue;
-`;
-
-const HdImg = styled.div`
-  grid-area: picture;
-  content: url(${(props) => props.picture});
-  background-size: contain;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: auto;
-  height: 100%;
-`;
 export default Articles;
