7a2e0cb6b8678e3e0cf376fa96b03bec653d014d
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-26 15:05:59 +0200

    [add] Events component with axios fetxhing calls from api

---
+++ src/components/Event.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Moment from "react-moment";
+import Modal from "react-modal";
+
+import {
+  DateContainer,
+  EventId,
+  EventUrl,
+  Location,
+  Description,
+  SingleEventContainer,
+  ThumbnailImg,
+  Title,
+} from "./layout/EventsElements";
+
+Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    margin: "0",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "black",
+    backgroundSize: "50%",
+    backgroundPosition: "center",
+  },
+};
+const Event = (props) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <React.Fragment>
+      <SingleEventContainer onClick={openModal}>
+        <EventId>#00{props.event.id}</EventId>
+        <Title>{props.event.name}</Title>
+        <Description>{props.event.description}</Description>
+        <Location>{props.event.location}</Location>
+        <ThumbnailImg picture={props.event.feature_image}></ThumbnailImg>
+        <DateContainer>
+          <Moment>{props.event.date}</Moment>
+        </DateContainer>
+        <EventUrl href={props.event.url}>{props.event.url}</EventUrl>
+      </SingleEventContainer>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Modal"
+        onClick={closeModal}
+      >
+        <p>{props.event.video_url}</p>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default Event;
