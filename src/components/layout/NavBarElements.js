4176c491bd4a60baa09bb4ad19fd776f4e8af5ac
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-29 17:37:09 +0200

    [update] colors according to theme

--- src/components/layout/NavBarElements.js
+++ src/components/layout/NavBarElements.js
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
-import logo from '../../static/img/logo/logo_white.png';
 
 export const NavContainer = styled.div`
   display: grid;
   grid-template-columns: 10% 80% 10%;
   grid-area: header;
-  border-bottom: 3px solid black;
+  border-bottom: 3px solid ${(props) => props.theme.borderColor};
   border-radius: 0 0 10px 10px;
-  background-color: white;
+  background-color: ${(props) => props.theme.headerBgColor};
   margin: 0 0.5vw;
 `;
 
-export const Logo = styled.img`
+export const Logo = styled.div`
   width: 95px;
   margin-left: 20px;
   height: 65px;
-  content:url(${logo});
-  text-align: center
+  background-image: url(${(props) => props.theme.logo});
+  background-size: contain;
+  text-align: center;
 `;
 
 export const NavbarOption = styled(Link)`
   font-size: 20px;
   font-weight: 370;
-  color: #100D80;
-  -webkit-text-stroke: 1px #100D80;
+  color: ${(props) => props.theme.textColor};
+  -webkit-text-stroke: 1px #100d80;
   letter-spacing: 1px;
 `;
