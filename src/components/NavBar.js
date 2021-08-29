4d7fef3f3f554cf4b9c033691008a7de00d877b6
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-29 17:36:12 +0200

    [add] themechanger function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,43 +3,58 @@ import { NavContainer } from "./layout/NavBarElements";
 import styled from "styled-components";
 import { NavbarOption, Logo } from "./layout/NavBarElements";
 
-function NavBar() {
+const NavBar = (props) => {
+  const changeTheme = () => {
+    if (props.theme === "light") {
+      props.setTheme("dark");
+    } else {
+      props.setTheme("light");
+    }
+  };
   return (
     <NavContainer>
-      <NavbarOption to="/" title="Home" theme={""}>
+      <NavbarOption to="/" title="Home">
         <Logo />
       </NavbarOption>
       <Links>
-        <NavbarOption to="/" title="Home" theme={""}>
+        <NavbarOption to="/" title="Home">
           Home
         </NavbarOption>
 
-        <NavbarOption to="/news" title="News" theme={""}>
+        <NavbarOption to="/news" title="News">
           News
         </NavbarOption>
 
-        <NavbarOption to="/gallery" title="Gallery" theme={""}>
+        <NavbarOption to="/gallery" title="Gallery">
           Gallery
         </NavbarOption>
 
-        <NavbarOption to="/spacecrafts" title="Spacecrafts" theme={""}>
+        <NavbarOption to="/spacecrafts" title="Spacecrafts">
           Spacecrafts
         </NavbarOption>
 
-        <NavbarOption to="/astronauts" title="Astronauts" theme={""}>
+        <NavbarOption to="/astronauts" title="Astronauts">
           Astronauts
         </NavbarOption>
-        <NavbarOption to="/events" title="Events" theme={""}>
+        <NavbarOption to="/events" title="Events">
           Events
         </NavbarOption>
 
-        <NavbarOption to="/locations" title="Locations" theme={""}>
+        <NavbarOption to="/locations" title="Locations">
           Locations
         </NavbarOption>
       </Links>
+      <ThemeChangeDiv>
+        <ThemeChangeButton onClick={changeTheme}>
+          {props.theme} mode
+        </ThemeChangeButton>
+      </ThemeChangeDiv>
     </NavContainer>
   );
-}
+};
+
+const ThemeChangeButton = styled.button``;
+const ThemeChangeDiv = styled.div``;
 
 const Links = styled.div`
   display: flex;
