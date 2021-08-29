a649af42a01741c50badcd190ec3351d29b51abf
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-29 22:00:56 +0200

    [add] navbar options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavContainer } from "./layout/NavBarElements";
 import styled from "styled-components";
 import { NavbarOption, Logo } from "./layout/NavBarElements";
-
+import {
+  faSun,
+  faMoon,
+  faUserAstronaut,
+  faCompressArrowsAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const NavBar = (props) => {
-  const changeTheme = () => {
-    if (props.theme === "light") {
-      props.setTheme("dark");
+  const [showMenu, setShowMenu] = useState(false);
+
+  const dropMenu = () => {
+    if (showMenu) {
+      setShowMenu(false);
     } else {
-      props.setTheme("light");
+      setShowMenu(true);
     }
   };
+
+  const changeTheme = (choosenTheme) => {
+    switch (choosenTheme) {
+      case "light":
+        props.setTheme("light");
+        break;
+      case "dark":
+        props.setTheme("dark");
+        break;
+      case "alien":
+        props.setTheme("alien");
+        break;
+      default:
+        props.setTheme("light");
+    }
+  };
+
   return (
     <NavContainer>
       <NavbarOption to="/" title="Home">
@@ -45,16 +70,72 @@ const NavBar = (props) => {
         </NavbarOption>
       </Links>
       <ThemeChangeDiv>
-        <ThemeChangeButton onClick={changeTheme}>
-          {props.theme} mode
+        <ThemeChangeButton icon={faCompressArrowsAlt} onClick={dropMenu}>
+          Change Theme
         </ThemeChangeButton>
+        {showMenu ? (
+          <DropdownMenu>
+            <ThemeButton icon={faSun} onClick={() => changeTheme("light")}>
+              {" "}
+              Light{" "}
+            </ThemeButton>
+            <ThemeButton icon={faMoon} onClick={() => changeTheme("dark")}>
+              {" "}
+              Dark{" "}
+            </ThemeButton>
+            <ThemeButton
+              icon={faUserAstronaut}
+              onClick={() => changeTheme("alien")}
+            >
+              {" "}
+              Alien{" "}
+            </ThemeButton>
+          </DropdownMenu>
+        ) : null}
       </ThemeChangeDiv>
     </NavContainer>
   );
 };
 
-const ThemeChangeButton = styled.button``;
-const ThemeChangeDiv = styled.div``;
+const ThemeChangeButton = styled.div`
+  display: flex;
+  width: 60px;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: black;
+  cursor: pointer;
+  border-radius: 20px;
+  border: 1px solid black;
+`;
+const ThemeButton = styled(FontAwesomeIcon)`
+  width: 30px !important;
+  height: 50px;
+  color: white;
+  outline: red;
+  -webkit-text-stroke: 1px solid red;
+  cursor: pointer;
+`;
+const DropdownMenu = styled.div`
+  position: absolute;
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 50px;
+  width: 80px;
+  flex-direction: column;
+  background-color: rgba(0, 0, 0, 0.5);
+  border: 2px solid black;
+  border-radius: 10px;
+  color: red;
+`;
+const ThemeChangeDiv = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Links = styled.div`
   display: flex;
