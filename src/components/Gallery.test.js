fc2adb6340f66dcecc245c375b1b485522f76cab
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-07 19:19:56 +0200

    [Add] initial tests with existing header test

---
+++ src/components/Gallery.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Gallery from "./Gallery";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+let getByTestId;
+
+beforeEach(() => {
+    const component = render(<Gallery/>);
+    getByTestId = component.getByTestId;
+})
+
+test("header renders with correct text", () => {
+    const headerElement = getByTestId("gallery-header");
+
+    expect(headerElement.textContent).toBe("Gallery");
+})
+
