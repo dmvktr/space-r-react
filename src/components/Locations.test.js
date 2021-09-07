c97033155829eeeb9f9abb4e7d15b08a4f9055f9
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-07 17:24:06 +0200

    [Add] test for Locations and implement header exist test

---
+++ src/components/Locations.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Locations from "./Locations";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+let getByTestId;
+
+beforeEach(() => {
+    const component = render(<Locations/>);
+    getByTestId = component.getByTestId;
+})
+
+test("header renders with correct text", () => {
+    const headerElement = getByTestId("locations-header");
+
+    expect(headerElement.textContent).toBe("Locations");
+})
+
