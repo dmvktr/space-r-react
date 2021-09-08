29e46afe4bdfb2c3759d1ef4428302549661ed1f
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-08 18:50:42 +0200

    [update] axios call replaced with data handler call [refactor] moved to directory package

--- src/components/locations/Pad.js
+++ src/components/locations/Pad.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PadContainer } from './layout/LocationElements';
+import { PadContainer } from '../layout/LocationElements';
 
 export const Pad = (props) => {
 
