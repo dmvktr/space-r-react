8fb784678e9d99e769984d5a9143623d114e55c2
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-08 18:52:19 +0200

    [refactor] navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,103 +1,103 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {
-  Logo,
-  DropdownMenu,
-  Links,
-  NavContainer,
-  ThemeButton,
-  ThemeChangeButton,
-  ThemeChangeDiv,
-  LogoContainer,
-  ResponsiveLinks
+    Logo,
+    DropdownMenu,
+    Links,
+    NavContainer,
+    ThemeButton,
+    ThemeChangeButton,
+    ThemeChangeDiv,
+    LogoContainer,
+    ResponsiveLinks
 } from "./layout/NavBarElements";
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import DropdownRoutes from "./DropdownRoutes";
 import NavLinks from "./NavLinks";
 import {faAtom, faMoon, faSun, faUserSecret, faBars} from "@fortawesome/free-solid-svg-icons";
+
 const NavBar = (props) => {
-  const [showMenu, setShowMenu] = useState(false);
-  const [isToggled, setIsToggled] = useState('none');
-  const doesMatchScreenWidth = useMediaQuery('(max-width: 68em)');
+    const [showMenu, setShowMenu] = useState(false);
+    const [isToggled, setIsToggled] = useState('none');
+    const doesMatchScreenWidth = useMediaQuery('(max-width: 68em)');
 
-  const toggleNavDropdown = (isToggled) => {
-    if(showMenu){
-      setShowMenu(false);
-    }
-    setIsToggled(isToggled ==='flex'? 'none': 'flex');
-  };
+    const toggleNavDropdown = (isToggled) => {
+        if (showMenu) {
+            setShowMenu(false);
+        }
+        setIsToggled(isToggled === 'flex' ? 'none' : 'flex');
+    };
 
-  const dropMenu = () => {
-    if (showMenu) {
-      setShowMenu(false);
-    } else if(isToggled){
-      setIsToggled('none');
-      setShowMenu(true);
-    } else{
-      setShowMenu(true);
-    }
-      
-  };
+    const dropMenu = () => {
+        if (showMenu) {
+            setShowMenu(false);
+        } else if (isToggled) {
+            setIsToggled('none');
+            setShowMenu(true);
+        } else {
+            setShowMenu(true);
+        }
+
+    };
 
 
+    const changeTheme = (choosenTheme) => {
+        switch (choosenTheme) {
+            case "light":
+                props.setTheme("light");
+                dropMenu()
+                break;
+            case "dark":
+                props.setTheme("dark");
+                dropMenu()
+                break;
+            case "alien":
+                props.setTheme("alien");
+                dropMenu()
+                break;
+            default:
+                props.setTheme("light");
+                dropMenu()
+        }
+    };
 
-  const changeTheme = (choosenTheme) => {
-    switch (choosenTheme) {
-      case "light":
-        props.setTheme("light");
-        dropMenu()
-        break;
-      case "dark":
-        props.setTheme("dark");
-        dropMenu()
-        break;
-      case "alien":
-        props.setTheme("alien");
-        dropMenu()
-        break;
-      default:
-        props.setTheme("light");
-        dropMenu()
-    }
-  };
-
-  return (
-    <NavContainer>
-      <LogoContainer to="/" title="Home">
-        <Logo />
-      </LogoContainer>
-      {doesMatchScreenWidth ? (<ResponsiveLinks display={isToggled}>
-        <NavLinks />
-      </ResponsiveLinks>) :
-      <Links>
-        <NavLinks />
-      </Links>
-      }
-      <ThemeChangeDiv>
-        <DropdownRoutes icon={faBars} onClick={toggleNavDropdown} display={isToggled} />
-        <ThemeChangeButton icon={faUserSecret} onClick={dropMenu}>
-        </ThemeChangeButton>
-        {showMenu ? (
-          <DropdownMenu>
-            <ThemeButton icon={faSun} onClick={() => changeTheme("light")}>
-              {" "}
-              Light{" "}
-            </ThemeButton>
-            <ThemeButton icon={faMoon} onClick={() => changeTheme("dark")}>
-              {" "}
-              Dark{" "}
-            </ThemeButton>
-            <ThemeButton
-              icon={faAtom}
-              onClick={() => changeTheme("alien")}
-            >
-              {" "}
-              Alien{" "}
-            </ThemeButton>
-          </DropdownMenu>
-        ) : null}
-      </ThemeChangeDiv>
-    </NavContainer>
-  );
+    return (
+        <NavContainer>
+            <LogoContainer to="/" title="Home">
+                <Logo data-testid="logo"/>
+            </LogoContainer>
+            {doesMatchScreenWidth ? (<ResponsiveLinks display={isToggled}>
+                    <NavLinks/>
+                </ResponsiveLinks>) :
+                <Links>
+                    <NavLinks/>
+                </Links>
+            }
+            <ThemeChangeDiv>
+                <DropdownRoutes icon={faBars} onClick={toggleNavDropdown} display={isToggled}/>
+                <ThemeChangeButton icon={faUserSecret} onClick={dropMenu} data-testid="themeChangerButton">
+                </ThemeChangeButton>
+                {showMenu ? (
+                    <DropdownMenu data-testid="themeChangeDiv">
+                        <ThemeButton icon={faSun} onClick={() => changeTheme("light")}>
+                            {" "}
+                            Light{" "}
+                        </ThemeButton>
+                        <ThemeButton icon={faMoon} onClick={() => changeTheme("dark")}>
+                            {" "}
+                            Dark{" "}
+                        </ThemeButton>
+                        <ThemeButton
+                            icon={faAtom}
+                            onClick={() => changeTheme("alien")}
+                        >
+                            {" "}
+                            Alien{" "}
+                        </ThemeButton>
+                    </DropdownMenu>
+                ) : null}
+            </ThemeChangeDiv>
+        </NavContainer>
+    );
 };
 
 export default NavBar;
