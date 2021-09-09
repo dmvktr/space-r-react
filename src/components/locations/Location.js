d1f6df6c85076c03fa4ee0635bb96f5b13717c3c
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-09 14:05:28 +0200

    [remove] unused property

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -3,7 +3,7 @@ import { LocationContainer, LocationCard } from '../layout/LocationElements';
 
 export const Location = (props) => {
     
-    const { landingPads, name, country_code, map_image, total_launch_count, total_landing_count } = props.location;
+    const { name, country_code, map_image, total_launch_count, total_landing_count } = props.location;
 
     return (
         <LocationContainer>
